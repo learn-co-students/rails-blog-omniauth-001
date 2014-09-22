a84d029d41cdf9de402ea4c8388d401d6aa91a29
ahimmelstoss <ahimmelstoss@gmail.com>
2014-09-22 14:28:25 -0400

    solution

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,11 @@ class User < ActiveRecord::Base
   has_many :posts
   has_many :comments
 
+  def self.create_with_omniauth(auth)
+    self.create(
+      :provider => auth["provider"],
+      :uid => auth["uid"],
+      :name => auth["info"]["name"]
+    )
+  end
 end
