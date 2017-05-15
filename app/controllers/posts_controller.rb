04562a6f3eca6d3c9666bf72e65e2a38318400ec
epang1 <epang1@gmail.com>
2017-05-15 17:51:18 -0400

    done

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :set_user, only: [:index, :show, :edit, :update, :destroy, :new]
 
   def index
     @posts = Post.all
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ActiveRecord::Base
   has_many :posts
   has_many :comments
 
+  def self.create_with_omniauth(auth)
+    User.create(name: auth[:info][:name], uid: auth[:uid], provider: auth[:provider])
+  end
+
 end
